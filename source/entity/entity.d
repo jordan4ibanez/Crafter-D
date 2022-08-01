05334f9621a7e28f68e7ea965e624c6f8a8a5232
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 02:27:18 -0400

    Add a base entity abstract class

--- source/entity/entity.d
+++ source/entity/entity.d
@@ -1,5 +1,11 @@
 module entity.entity;
 
+import raylib;
+
+// A raw abstract class which all entity sub-types will inherit
 abstract class Entity {
-    
+    // Entities must have spatial fields
+    Vector3 position = Vector3(0,0,0);
+    Vector3 inertia  = Vector3(0,0,0);
+    Vector3 rotation = Vector3(0,0,0);
 }
