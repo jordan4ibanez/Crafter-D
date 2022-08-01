dcf5ea1ae9af3e1e4cd6376d5c23e380db162988
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 02:29:15 -0400

    Minor wording

--- source/entity/entity.d
+++ source/entity/entity.d
@@ -2,7 +2,7 @@ module entity.entity;
 
 import raylib;
 
-// A raw abstract class which all entity sub-types will inherit
+// The base abstract class which all entity sub-types will inherit
 abstract class Entity {
     // Entities must have spatial fields
     Vector3 position = Vector3(0,0,0);
