f5ed70a78e8d180b824858064f4dcd2e71fef5bd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 03:30:58 -0400

    Test static mob factory

--- source/app.d
+++ source/app.d
@@ -3,12 +3,8 @@ import std.stdio;
 import raylib;
 import entity.mob.mob;
 import entity.mob.zombie;
+import entity.mob.mob_factory;
 
-void main()
-{
-	Mob[] debugBoi;
-
-    debugBoi ~= new Zombie();
-
-    debugBoi[0].onSpawn();
+void main() {
+	MobFactory.spawnMob(new Zombie());
 }
