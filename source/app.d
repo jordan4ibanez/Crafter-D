389fd47591601e4918dec60eedd1a55af7288295
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 02:42:30 -0400

    Debug first mob

--- source/app.d
+++ source/app.d
@@ -1,38 +1,14 @@
 import std.stdio;
 
 import raylib;
+import entity.mob.mob;
+import entity.mob.zombie;
 
 void main()
 {
 	Mob[] debugBoi;
 
     debugBoi ~= new Zombie();
-}
 
-
-class Zombie : Mob {
-
-    this() {
-        this.health = 10;
-    }
-
-    override void onSpawn() {
-
-    }
-
-    override void onTick(double delta) {
-
-    }
-
-    override void onHurt() {
-
-    }
-
-    override void onDeath() {
-
-    }
-
-    override void onDeathPoof() {
-
-    }
+    debugBoi[0].onSpawn();
 }
