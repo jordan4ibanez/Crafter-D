bf2db4511ad1e92caf2d7850df87962dfa14be0d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 03:17:20 -0400

    Entity UUID,Dimensions,Physical

--- source/entity/entity.d
+++ source/entity/entity.d
@@ -6,13 +6,24 @@ import std.stdio;
 
 // The base abstract class which all entity sub-types will inherit
 abstract class Entity {
+
     // Entities must have spatial fields
     private Vector3 position = Vector3(0,0,0);
     private Vector3 inertia  = Vector3(0,0,0);
     private Vector3 rotation = Vector3(0,0,0);
 
+    // Entities have a width/depth (x) and a height (y)
+    private Vector2 dimensions = Vector2(0,0);
+
+    // Entities must have a physical state, collision detection with environment
+    private bool physical = true;
+
+    // Entities must be unique
+    private UUID uuid;
+
     this() {
         writeln("I am creating an entity object");
+        this.setUUID();
     }
 
 
@@ -34,5 +45,38 @@ abstract class Entity {
     }
     void setRotation(Vector3 newRotation) {
         this.rotation = newRotation;
+    }
+    UUID getUUID() {
+        return this.uuid;
+    }
+    void setUUID() {
+        // This is special, it is a one way lock for the UUID
+        if (this.uuid.empty()) {
+            this.uuid = randomUUID();
+        }
+    }
+    Vector2 getDimensions() {
+        return this.dimensions;
+    }
+    void setDimensions(Vector2 newDimensions) {
+        this.dimensions = newDimensions;
+    }
+    double getWidth() {
+        return this.dimensions.x;
+    }
+    void setWidth(double newWidth) {
+        this.dimensions.x = newWidth;
+    }
+    double getHeight() {
+        return this.dimensions.y;
+    }
+    void setHeight(double newHeight) {
+        this.dimensions.y = newHeight;
+    }
+    bool getPhysical() {
+        return this.physical;
+    }
+    void setPhysical(bool newPhysical) {
+        this.physical = newPhysical;
     }
 }
