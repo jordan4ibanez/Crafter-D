3e07e20f72d07d1e693cc2355d75858ad265c498
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 03:30:39 -0400

    Add more documentation

--- source/entity/mob/mob.d
+++ source/entity/mob/mob.d
@@ -7,8 +7,10 @@ import raylib;
 // The base class for all mobs
 abstract class Mob : Entity {
 
+    // All Mobs must have health
     private float health;
 
+    // All mobs must implement these methods 
     abstract void onSpawn();
     abstract void onTick(double delta);
     abstract void onHurt();
