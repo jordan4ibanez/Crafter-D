cfbc88202ce48a1feb46cd74f28d2aa0f1c40270
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 20:06:49 -0400

    Give skeleton "skeleton" of class

--- source/entity/mob/mob_definitions/skeleton.d
+++ source/entity/mob/mob_definitions/skeleton.d
@@ -1,2 +1,34 @@
 module entity.mob.mob_definitions.skeleton;
 
+import entity.mob.mob;
+import std.stdio;
+
+public class Skeleton : Mob {
+
+    this() {
+        super();
+
+        // writeln("I am creating a zombie object");
+        // writeln("My UUID is: ", this.getUUID());
+    }
+
+    override void onSpawn() {
+        
+    }
+
+    override void onTick(double delta) {
+        
+    }
+
+    override void onHurt() {
+        
+    }
+
+    override void onDeath() {
+        
+    }
+
+    override void onDeathPoof() {
+        
+    }
+}
