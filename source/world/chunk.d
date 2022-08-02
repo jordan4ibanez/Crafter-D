da07f1fd6392279414d04cd7c086f21968ea9b21
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 23:39:31 -0400

    Create chunk.d

---
+++ source/world/chunk.d
@@ -0,0 +1,107 @@
+module world.chunk;
+
+import raylib;
+import std.stdio;
+
+/*
+Notes:
+
+Chunks are technically 1D in memory.
+
+They utilize 1D to 3D spatial striping to be fast.
+
+0,0,0 starts at index 0. 15,127,15 is at index 32768.
+
+ */
+
+
+// Pre-calculation
+const int xSize = 16;
+const int ySize = 128;
+const int zSize = 16;
+
+const int chunkArrayLength = xSize * ySize * zSize;
+const int yStride = xSize * ySize;
+
+// Micro struct for exact chunk math
+struct Vector3I {
+    int x = 0;
+    int y = 0;
+    int z = 0;
+}
+
+// 1D index to Vector3 position
+Vector3I indexToPosition(int index) {
+    return Vector3I(
+         index / yStride,
+        (index % yStride) % ySize,
+        (index % yStride) / ySize
+    );
+}
+
+// Vector3 position to 1D index
+int positionToIndex(Vector3I position) {
+    return (position.x * yStride) + (position.z * ySize) + position.y;
+}
+
+// Overload
+int positionToIndex(int x, int y, int z) {
+    return (x * yStride) + (z * ySize) + y;
+}
+
+// Basic inline collision detection
+bool collideX(int value) {
+    return (value >= 0 && value < xSize);
+}
+bool collideY(int value) {
+    return (value >= 0 && value < ySize);
+}
+bool collideZ(int value) {
+    return (value >= 0 && value < zSize);
+}
+// All at once
+bool collide(int x, int y, int z) {
+    return (collideX(x) && collideY(y) && collideZ(z));
+}
+
+public class Chunk {
+    private int[chunkArrayLength]  block;
+    private byte[chunkArrayLength] light;
+    private byte[chunkArrayLength] rotation;
+    // Height map needs to be added in
+
+    private string biome;
+
+    this(string biomeName) {
+        this.setBiome(biomeName);
+    }
+
+
+    int getBlock(int x, int y, int z) {
+        if (collide(x,y,z)) {
+            return(this.block[positionToIndex(x,y,z)]);
+        } else {
+            // failed for some reason
+            writeln("Getblock FAILED!");
+            return -1;
+        }
+    }
+    int getBlock(int index) {
+        return this.block[index];
+    }
+    void setBlock(int x, int y, int z, int newBlock) {
+        if (collide(x,y,z)) {
+            this.block[positionToIndex(x,y,z)] = newBlock;
+        }
+    }
+    void setBlock(int index, int newBlock) {
+        this.block[index] = newBlock;
+    }
+
+    string getBiome() {
+        return this.biome;
+    }
+    void setBiome(string newBiome) {
+        this.biome = newBiome;
+    }
+}
