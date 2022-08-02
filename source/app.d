7cebe38d23504d780bd2bc6eb903f7e9733270f1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 21:21:10 -0400

    Add more debug

--- source/app.d
+++ source/app.d
@@ -4,9 +4,16 @@ import raylib;
 import entity.mob.mob;
 import entity.mob.mob_definitions.zombie;
 import entity.mob.mob_factory;
+import entity.player.player;
+import entity.player.player_factory;
 
 void main() {
+    // Debug mobs
 	MobFactory.spawnMob(new Zombie());
-
     MobFactory.debugFactory();
+
+    // Debug players
+    PlayerFactory.spawnPlayer(new Player("singleplayer"));
+    PlayerFactory.debugFactory();
+
 }
