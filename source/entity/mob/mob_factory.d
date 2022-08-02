2e3e18dcc45785e7e967f63202ac9043ae5fee3d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-01 20:06:26 -0400

    Add mob class info when added to static factory

--- source/entity/mob/mob_factory.d
+++ source/entity/mob/mob_factory.d
@@ -3,14 +3,13 @@ module entity.mob.mob_factory;
 import std.stdio;
 import std.uuid;
 import entity.mob.mob;
-import entity.mob.mob_definitions.zombie;
 
 public static final class MobFactory {
     private static Mob[UUID] container;
 
     public static void spawnMob(Mob newMob) {
         container[newMob.getUUID()] = newMob;
-        writeln("MOB HAS BEEN ADDED TO FACTORY: ", newMob.getUUID());
+        writeln(newMob.classinfo.name , " HAS BEEN ADDED TO FACTORY: ", newMob.getUUID());
     }
 
     public static void debugFactory() {
