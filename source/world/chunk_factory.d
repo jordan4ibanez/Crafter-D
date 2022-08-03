3b7d646da6fe8675e4cb04813d6363939f63c63d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-02 22:17:39 -0400

    Write out basics of chunk factory

--- source/world/chunk_factory.d
+++ source/world/chunk_factory.d
@@ -1,6 +1,66 @@
 module world.chunk_factory;
 
+
+import std.stdio;
+import world.chunk;
+import std.container.dlist;
+import world.chunk_structs;
+import std.range : popFront, popBack;
+import std.algorithm : canFind;
+
 // This handles the chunks in the world. A static factory/container for Chunks
+// This is meant to be handled functionally
 public static class ChunkFactory {
 
+    // Hashmap container for generated chunks
+    private static Chunk[Position2I] container;
+
+    // Generation stack
+    private static Position2I[] stack;
+
+    // Entry point into adding new chunks to the map
+    public static void newChunkGeneration(int x, int z) {
+        Position2I newGenerationPosition = Position2I(x,z);
+
+        // Do not dispatch a new chunk generation into stack if it's already there
+        if (!this.stack.canFind(newGenerationPosition)) {
+            stack ~= newGenerationPosition;
+        }
+    }
+
+    // Will poll the generation stack 
+    public static void processStack() {
+
+        // See if there are any new chunk generations
+        if (this.stack.length > 0) {
+
+            Position2I poppedValue = this.stack[0];
+            this.stack.popFront();
+            writeln("popped: ", poppedValue);
+
+            // Ship them to the chunk generator process
+            generateChunk(poppedValue);
+        }
+    }
+
+    private static void generateChunk(Position2I newPosition) {
+        writeln("I'm generating a new chunk at: ", newPosition);
+
+        // random debug for prototyping processes
+        Chunk generatedChunk = new Chunk("default", newPosition);
+
+        // Generation process goes here
+        // It will take the object, do work with it, then give it back
+
+        // Finally insert the chunk into the container
+        this.container[newPosition] = generatedChunk;
+
+        // The factory is now done processing the chunk
+    }
+
+
+    // Everything past this is for debugging components of this static class
+    public static void debugFactoryContainer() {
+        writeln("factory container: ", this.container);
+    }
 }
