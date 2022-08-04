e2bdf73669f8bc01fb86862542e50e099b7115b5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-03 21:32:09 -0400

    Add the ability for chunks to generate a 2D real noise value

--- source/world/world_generation.d
+++ source/world/world_generation.d
@@ -3,6 +3,7 @@ module world.world_generation;
 import std.stdio;
 import world.chunk;
 import fast_noise;
+import std.math.rounding;
 
 // This is another static factory class. Used for, you guessed it, world generation
 public static class WorldGenerator {
@@ -11,6 +12,7 @@ public static class WorldGenerator {
 
     // IMPORTANT: THIS NEEDS TO DO WORK ON A CHUNK!
     public static void generate (int chunkX, int chunkZ) {
+
         FNLState noise = fnlCreateState(this.seed);
         noise.noise_type = FNLNoiseType.FNL_NOISE_OPENSIMPLEX2S;
 
@@ -18,8 +20,39 @@ public static class WorldGenerator {
         int basePositionX = chunkX * 16;
         int basePositionZ = chunkZ * 16;
 
-        //for (int x = )
+        // These will be defined in a biome container
 
+        // The base height of the chunk
+        int baseHeight = 70;
+        // How high or low it can fluctuate based on the noise (-1 to 1)
+        int fluxHeight = 20;
+
+        // Iterate the 2D noise of the chunk
+        for (int x = 0; x < chunkSizeX; x++) {
+            for (int z = 0; z < chunkSizeZ; z++) {
+
+                // The real position in 2D space
+                int currentPositionX = x + basePositionX;
+                int currentPositionZ = z + basePositionZ;
+
+                // Noise at position
+                float currentNoise = fnlGetNoise2D(&noise, currentPositionX, currentPositionZ);
+
+                // Get the height fluctuation of the current position
+                int currentHeightFlux = cast(int)floor(fluxHeight * currentNoise);
+
+                // Now add it to the defined baseHeight of the biome
+                int realHeight = baseHeight + currentHeightFlux;
+
+                // Debug
+                writeln("the height at ", currentPositionX, ",", currentPositionZ, " is ", realHeight);
+
+                // Here will go a stack fill with predefined layers and whatnot
+            }
+        }
+
+
+        // This is the cavegen prototype, this is going to take a lot of tuning
         /*
         for (int i = 0; i < chunkArrayLength; i++) {
             Vector3I currentPosition = indexToPosition(i);
