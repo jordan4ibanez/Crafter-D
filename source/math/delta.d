6bc6616e024e5deb23dae570f3d5e9e773b28e6a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-04 22:28:12 -0400

    Implement high precision delta calculation

---
+++ source/math/delta.d
@@ -0,0 +1,27 @@
+module math.delta;
+
+import core.time;
+
+public static class Delta {
+
+    private static MonoTime before = MonoTime.zero;
+    private static MonoTime after = MonoTime.zero;
+
+    // High precision delta for users with powerful computers
+    private static double delta = 0;
+
+    // Calculates the delta time between frames. First will always be 0
+    public static void calculateDelta() {
+        this.after = MonoTime.currTime;
+        Duration duration = after - before;
+        this.delta = cast(double)duration.total!("nsecs") / 1_000_000_000.0;
+        this.before = MonoTime.currTime;
+    }
+
+    public static double getDelta() {
+        return this.delta;
+    }
+}
+
+alias getDelta = Delta.getDelta;
+alias calculateDelta = Delta.calculateDelta;
