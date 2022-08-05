bd2014647fc27f38279a28306d8ab165040a7eeb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-04 22:46:47 -0400

    Allow users to use --server flag to begin server loop

--- source/app.d
+++ source/app.d
@@ -10,59 +10,80 @@ import world.chunk;
 import world.chunk_factory;
 import world.chunk;
 import world.world_generation;
-
-void main() {
-    //WorldGenerator.generate(128,0);
-
-    /*
-    // Debug mobs
-	MobFactory.spawnMob(new Zombie());
-    MobFactory.debugFactory();
-
-    // Debug players
-    PlayerFactory.spawnPlayer(new Player("singleplayer"));
-    PlayerFactory.debugFactory();
+import raymath;
+import math.delta;
 
 
-    ChunkFactory.newChunkGeneration(0,0);
-    ChunkFactory.processStack();
-    ChunkFactory.debugFactoryContainer();
 
-    //Chunk chunky = new Chunk("default", Position2I(0,0));
-    for ( int i = 0; i < 32_768; i++ ) {
-        chunky.runADebug(i);
+void main(string[] args) {
+
+    if (args.length > 1 && args[1] == "--server") {
+        // Server loop
+        bool serverShouldClose = false;
+
+        while (!serverShouldClose) {
+            calculateDelta();
+            writeln("wow this is a server! ", getDelta());
+        }
+    } else {
+        //WorldGenerator.generate(128,0);
+
+        /*
+        // Debug mobs
+        MobFactory.spawnMob(new Zombie());
+        MobFactory.debugFactory();
+
+        // Debug players
+        PlayerFactory.spawnPlayer(new Player("singleplayer"));
+        PlayerFactory.debugFactory();
+
+
+        ChunkFactory.newChunkGeneration(0,0);
+        ChunkFactory.processStack();
+        ChunkFactory.debugFactoryContainer();
+
+        //Chunk chunky = new Chunk("default", Position2I(0,0));
+        for ( int i = 0; i < 32_768; i++ ) {
+            chunky.runADebug(i);
+        }
+        */
+
+
+
+        InitWindow(1280,720, "Voxel Thing");
+        //SetTargetFPS(60);
+
+        // Debug camera
+        Camera camera = Camera(Vector3(10,0,0), Vector3(0,0,0),Vector3(0,1,0),45, CameraProjection.CAMERA_PERSPECTIVE);
+
+        SetCameraMode(camera, CameraMode.CAMERA_FIRST_PERSON);
+
+
+
+        // Client loop
+        while(!WindowShouldClose()) {
+
+            // Delta calculation must come first
+            calculateDelta();
+
+
+
+            BeginDrawing();
+
+            ClearBackground(Colors.RAYWHITE);
+
+
+            UpdateCamera(&camera);
+
+            BeginMode3D(camera);
+
+            DrawSphere(Vector3(0,0,0),2, Colors.BLACK);
+
+            EndMode3D();
+
+            EndDrawing();
+        }
+
+        CloseWindow();
     }
-    */
-
-
-
-    InitWindow(1280,720, "Voxel Thing");
-    SetTargetFPS(60);
-
-    // Debug camera
-    Camera camera = Camera(Vector3(10,0,0), Vector3(0,0,0),Vector3(0,1,0),45, CameraProjection.CAMERA_PERSPECTIVE);
-
-    SetCameraMode(camera, CameraMode.CAMERA_FIRST_PERSON);
-
-
-    while(!WindowShouldClose()) {
-        BeginDrawing();
-
-
-        ClearBackground(Colors.RAYWHITE);
-
-
-        UpdateCamera(&camera);
-
-        BeginMode3D(camera);
-
-        DrawSphere(Vector3(0,0,0),2, Colors.BLACK);
-
-        EndMode3D();
-
-        EndDrawing();
-    }
-
-
-
 }
