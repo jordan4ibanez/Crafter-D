3e47748f28ef43fe533d28db5162b539f170dd33
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-11 18:38:14 -0400

    Set out goals for the second iteration

--- source/graphics/block_graphics_take_two.d
+++ source/graphics/block_graphics_take_two.d
@@ -12,3 +12,24 @@ const double textureMapTiles = 32;
 const double textureTileSize = 16;
 // Defines the total width/height of the texture map in pixels
 const double textureMapSize = textureTileSize * textureMapTiles;
+
+/*
+
+GOAL OF SECOND ITERATION:
+
+1. Simplify
+- The functional api needs to be easier to follow
+
+2. Code reduction
+- The code needs to have less repetition
+
+3. Data reduction
+- The computer should process less data to get the same result
+
+4. Code quality
+- Each function should have a goal and achieve that goal well
+
+5. Code reduction
+- The code needs to have less repetition
+
+*/
