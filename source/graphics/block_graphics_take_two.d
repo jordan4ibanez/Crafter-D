cc77d7a5010a5a28c15a5b7ea46da3fe84e92c1d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-11 04:24:10 -0400

    Add in important things

--- source/graphics/block_graphics_take_two.d
+++ source/graphics/block_graphics_take_two.d
@@ -1,2 +1,14 @@
 module graphics.block_graphics_take_two;
 
+import std.stdio;
+import raylib;
+import helpers.structs;
+import std.math: abs;
+
+
+// Defines how many textures are in the texture map
+const double textureMapTiles = 32;
+// Defines the width/height of each texture
+const double textureTileSize = 16;
+// Defines the total width/height of the texture map in pixels
+const double textureMapSize = textureTileSize * textureMapTiles;
