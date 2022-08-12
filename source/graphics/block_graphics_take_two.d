9e954b981ab893d3187fa800bdd8bef71975e7d2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-11 20:10:09 -0400

    Add documentation on blockboxes

--- source/graphics/block_graphics_take_two.d
+++ source/graphics/block_graphics_take_two.d
@@ -61,11 +61,47 @@ So [ 3, 0, 1 ] is the top left tri, and [ 1, 2, 3 ] is the bottom right tri.
 
 OpenGL will complete the triangle. So in tri 1, it automatically connects 1 to 3.
 
-We are using this counter clockwise wound tri in this form so the gpu can leverage
+This is using this counter clockwise wound tri in this form so the gpu can leverage
 pointer index reuse for the majority of the quad, and keep a mostly linear iteration.
 This may seem like a micro optimization, but for millions of faces, this will make
 quite a noticeable difference. The only outlier in this is the initial position.
 
 
 
+GOAL OF BLOCKBOX:
+
+A "blockbox" as I'm calling it, is a re-implementation of Minetest's "nodebox" as I've
+studied it. The basic gist of a block box is to allow a floating point boundary between
+0 (because blocks are based in position 0 on all axis) and 1 (which is the max that they
+can reach without running into another block). The basic implementation will be this:
+
+A block box consists of arrays of floating point numbers.
+
+[
+    [minx, miny, minz, maxx, maxy, maxz]... so on and so forth
+]
+
+If you wanted a normal drawtype blockbox, it would be like so:
+[
+    [0,0,0,1,1,1]
+]
+Though, this is not recommended, as I am still deciding on whether or not to cull out
+blockbox faces when they are exactly on the edge matching up with a normal block.
+
+A simple staircase would be like so:
+[
+    [0,0,0,1,0.5,1], // The base of the stair
+    [0,0,0,0.5,1,1]  // The top step
+]
+
+The blockbox will also have a built in bounds check when it is registered through a block
+registration (the world portion, this is the graphics portion of it).
+
+Another major goal of the block box is to automate the texturing from the base texture.
+So if you had a half height, or half width block, it would automatically show exactly half
+the pixels without stretching or squishing. Basically like you are cutting the texture out
+of the texture map pixel perfect and putting it on the face of the block, no matter the rotation.
+
 */
+
+
