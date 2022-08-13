dfda8371fece2dc4fa217ee518976e8999188188
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-13 18:45:23 -0400

    Create basis for chunk mesh generation

--- source/graphics/chunk_mesh_generation.d
+++ source/graphics/chunk_mesh_generation.d
@@ -1,4 +1,11 @@
 module graphics.chunk_mesh_generation;
 
 import raylib;
+import graphics.block_graphics;
+import world.chunk;
 
+
+// This is an interface to the block graphics
+Mesh generateChunkMesh(Chunk chunk) {
+    
+}
