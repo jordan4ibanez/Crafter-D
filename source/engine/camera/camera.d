c4b4c021851578ca3c40dfdf3547874292d22b47
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-27 23:09:29 -0400

    Update the camera speed

--- source/engine/camera/camera.d
+++ source/engine/camera/camera.d
@@ -39,25 +39,28 @@ Matrix4d getCameraMatrix() {
 }
 
 void testCameraHackRemoveThis() {
+
+    double speed = 100;
+
     // This is an extreme hack for testing remove this garbage
     Vector3d modifier = Vector3d(0,0,0);
 
     if(Keyboard.getForward()){
-        modifier.z -= getDelta() * 10;
+        modifier.z -= getDelta() * speed;
     } else if (Keyboard.getBack()) {
-        modifier.z += getDelta() * 10;
+        modifier.z += getDelta() * speed;
     }
 
     if(Keyboard.getLeft()){
-        modifier.x += getDelta() * 10;
+        modifier.x += getDelta() * speed;
     } else if (Keyboard.getRight()) {
-        modifier.x -= getDelta() * 10;
+        modifier.x -= getDelta() * speed;
     }
 
     if (Keyboard.getUp()){
-        modifier.y += getDelta() * 10;
+        modifier.y += getDelta() * speed;
     } else if (Keyboard.getDown()) {
-        modifier.y -= getDelta() * 10;
+        modifier.y -= getDelta() * speed;
     }
 
     movePosition(modifier);
