af04ab72e7609d6787015007bc5b7778344aff77
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-29 01:58:42 -0400

    Implement thread reservation for mesh generator

--- source/engine/thread/thread_library.d
+++ source/engine/thread/thread_library.d
@@ -10,7 +10,9 @@ import asdf;
 // The locks are important in case anyone starts modding the game, DO NOT want these getting changed
 
 private Tid worldGenerator;
+private Tid meshGenerator;
 private bool worldGeneratorLock = false;
+private bool meshGeneratorLock = false;
 
 void setWorldGeneratorThread(Tid worldGenThread) {
     if (!worldGeneratorLock) {
@@ -22,6 +24,14 @@ Tid getWorldGeneratorThread() {
     return worldGenerator;
 }
 
+void setMeshGeneratorThread(Tid meshGenThread) {
+    if (!meshGeneratorLock) {
+        meshGeneratorLock = true;
+        meshGenerator = meshGenThread;
+    }
+}
+
 void killAllThreads() {
     send(worldGenerator, true);
+    send(meshGenerator, true);
 }
