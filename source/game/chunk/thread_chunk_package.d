6a09d296652111370093ce1113fd7e5c3f106e8f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-29 20:24:26 -0400

    Implement basics of chunkpackage

--- source/game/chunk/thread_chunk_package.d
+++ source/game/chunk/thread_chunk_package.d
@@ -1,2 +1,26 @@
 module game.chunk.thread_chunk_package;
 
+import game.chunk.chunk;
+
+struct ThreadChunkPackage {
+    bool exists = false;
+    Chunk thisChunk;
+
+    Chunk neighborNegativeX;
+    Chunk neighborPositiveX;
+    Chunk neighborNegativeZ;
+    Chunk neighborPositiveZ;
+
+    this(Chunk thisChunk,
+         Chunk neighborNegativeX,
+         Chunk neighborPositiveX,
+         Chunk neighborNegativeZ,
+         Chunk neighborPositiveZ) {
+            this.thisChunk = thisChunk;
+            this.neighborNegativeX = neighborNegativeX;
+            this.neighborPositiveX = neighborPositiveX;
+            this.neighborNegativeZ = neighborNegativeZ;
+            this.neighborPositiveZ = neighborPositiveZ;
+            this.exists = true;
+        }
+}
