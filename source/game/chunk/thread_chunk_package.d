3f0a31f097b705b10512bb6601a844333cd73cf8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-29 21:39:05 -0400

    Add new info & boolean

--- source/game/chunk/thread_chunk_package.d
+++ source/game/chunk/thread_chunk_package.d
@@ -3,10 +3,14 @@ module game.chunk.thread_chunk_package;
 import game.chunk.chunk;
 import game.chunk.thread_message_chunk;
 
+// This is used strictly for the chunk mesh generator!
+// It's sole purpose is to tell the chunkmesh generator what the world is
+// so it can create mesh data
 shared struct ThreadChunkPackage {
     bool exists = false;
     ubyte yStack = 0;
     ThreadMessageChunk thisChunk;
+    bool updating = false;
 
     ThreadMessageChunk neighborNegativeX;
     ThreadMessageChunk neighborPositiveX;
@@ -19,7 +23,8 @@ shared struct ThreadChunkPackage {
          Chunk neighborPositiveX,
          Chunk neighborNegativeZ,
          Chunk neighborPositiveZ,
-         ubyte yStack) {
+         ubyte yStack,
+         bool updating) {
             this.thisChunk = ThreadMessageChunk(thisChunk);
             this.neighborNegativeX = ThreadMessageChunk(neighborNegativeX);
             this.neighborPositiveX = ThreadMessageChunk(neighborPositiveX);
@@ -27,5 +32,6 @@ shared struct ThreadChunkPackage {
             this.neighborPositiveZ = ThreadMessageChunk(neighborPositiveZ);
             this.exists = true;
             this.yStack = yStack;
+            this.updating = updating;
     }
 }
