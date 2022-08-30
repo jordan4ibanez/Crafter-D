0ed03a99c6ed87ed70b18cf1f632c0293313f360
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-29 22:05:11 -0400

    Re-engineer the chunk mesh generator

--- source/game/graphics/chunk_mesh_generator.d
+++ source/game/graphics/chunk_mesh_generator.d
@@ -45,47 +45,53 @@ That's about it really
 // Thread spawner starts here
 void startMeshGeneratorThread(Tid parentThread) {
 
-
-writeln("Starting thread mesh generator");
-
-
 // Gotta tell the main thread what has been created
 Tid mainThread = parentThread;
 
+writeln("Starting thread mesh generator");
+
+bool didGenLastLoop = false;
+
 // New meshes call this update to fully update neighbors on heap
 // This needs to be a package of current and neighbors
-Vector3i[] newStack = new Vector3i[0];
+ThreadChunkPackage[] newStack = new ThreadChunkPackage[0];
 
 // Preexisting meshes call this update to only update necessary neighbors on heap
 // This needs to be a package of current and neighbors
-Vector3i[] updatingStack = new Vector3i[0];
+ThreadChunkPackage[] updatingStack = new ThreadChunkPackage[0];
 
-void newChunkMeshUpdate(Vector3i position) {
-    if (!newStack.canFind(position)) {
-        newStack.insertInPlace(0, position);
-        // newStack ~= position;
+// This 
+void newChunkMeshUpdate(ThreadChunkPackage thisPackage) {
+    if (!newStack.canFind(thisPackage)) {
+        // newStack.insertInPlace(0, thisPackage);
+        newStack ~= thisPackage;
     }
 }
 
-void updateChunkMesh(Vector3i position) {
-    if (!updatingStack.canFind(position)) {
-        // newStack.insertInPlace(0, position);
-        updatingStack ~= position;
+void updateChunkMesh(ThreadChunkPackage thisPackage) {
+    if (!updatingStack.canFind(thisPackage)) {
+        updatingStack ~= thisPackage;
     }
 }
 
+void internalGenerateChunkMesh(ThreadChunkPackage thePackage) {
 
-void internalGenerateChunkMesh(Vector3i position) {
+    Chunk thisChunk = *new Chunk(thePackage.thisChunk);
 
-    Chunk thisChunk = getChunk(Vector2i(position.x, position.z));
+    Vector3i position = Vector3i(
+        thisChunk.getPosition().x,
+        thePackage.yStack,
+        thisChunk.getPosition().y
+    );
 
     // Get chunk neighbors
     // These do not exist by default
-    Chunk neighborNegativeX = getChunk(Vector2i(position.x - 1, position.z));
-    Chunk neighborPositiveX = getChunk(Vector2i(position.x + 1, position.z));
-    Chunk neighborNegativeZ = getChunk(Vector2i(position.x, position.z - 1));
-    Chunk neighborPositiveZ = getChunk(Vector2i(position.x, position.z + 1));
+    Chunk neighborNegativeX = *new Chunk(thePackage.neighborNegativeX);
+    Chunk neighborPositiveX = *new Chunk(thePackage.neighborPositiveX);
+    Chunk neighborNegativeZ = *new Chunk(thePackage.neighborNegativeZ);
+    Chunk neighborPositiveZ = *new Chunk(thePackage.neighborPositiveZ);
 
+    writeln("go");
     generateChunkMesh(
         thisChunk,
         neighborNegativeX,
@@ -94,32 +100,43 @@ void internalGenerateChunkMesh(Vector3i position) {
         neighborPositiveZ,
         cast(ubyte)position.y
     );
+    writeln("stop");
 
     // Update neighbors
     if (neighborNegativeX.exists()) {
-        updateChunkMesh(Vector3i(position.x - 1, position.y, position.z));
+        // updateChunkMesh(Vector3i(position.x - 1, position.y, position.z));
+        writeln("send out request for update!");
     }
     if (neighborPositiveX.exists()) {
-        updateChunkMesh(Vector3i(position.x + 1, position.y, position.z));
+        // updateChunkMesh(Vector3i(position.x + 1, position.y, position.z));
+        writeln("send out request for update!");
     }
     if (neighborNegativeZ.exists()) {
-        updateChunkMesh(Vector3i(position.x, position.y, position.z - 1));
+        // updateChunkMesh(Vector3i(position.x, position.y, position.z - 1));
+        writeln("send out request for update!");
     }
     if (neighborPositiveZ.exists()) {
-        updateChunkMesh(Vector3i(position.x, position.y, position.z + 1));
+        // updateChunkMesh(Vector3i(position.x, position.y, position.z + 1));
+        writeln("send out request for update!");
     }
 }
 
-void internalUpdateChunkMesh(Vector3i position) {
+void internalUpdateChunkMesh(ThreadChunkPackage thePackage) {
 
-    // This will use a serialized package soon
-    Chunk thisChunk = getChunk(Vector2i(position.x, position.z));
+    Chunk thisChunk = *new Chunk(thePackage.thisChunk);
+
+    Vector3i position = Vector3i(
+        thisChunk.getPosition().x,
+        thePackage.yStack,
+        thisChunk.getPosition().y
+    );
+
     // Get chunk neighbors
     // These do not exist by default
-    Chunk neighborNegativeX = getChunk(Vector2i(position.x - 1, position.z));
-    Chunk neighborPositiveX = getChunk(Vector2i(position.x + 1, position.z));
-    Chunk neighborNegativeZ = getChunk(Vector2i(position.x, position.z - 1));
-    Chunk neighborPositiveZ = getChunk(Vector2i(position.x, position.z + 1));
+    Chunk neighborNegativeX = *new Chunk(thePackage.neighborNegativeX);
+    Chunk neighborPositiveX = *new Chunk(thePackage.neighborPositiveX);
+    Chunk neighborNegativeZ = *new Chunk(thePackage.neighborNegativeZ);
+    Chunk neighborPositiveZ = *new Chunk(thePackage.neighborPositiveZ);
 
     generateChunkMesh(
         thisChunk,
@@ -137,39 +154,73 @@ void processChunkMeshUpdateStack(){
     // See if there are any new chunk generations
     if (newStack.length > 0) {
 
-        Vector3i poppedValue = newStack[0];
+        ThreadChunkPackage newPackage = newStack[0];
         newStack.popFront();
         // writeln("New Chunk Mesh: ", poppedValue);
 
         // Ship them to the chunk generator process
-        internalGenerateChunkMesh(poppedValue);
+        internalGenerateChunkMesh(newPackage);
     }
     
     // See if there are any existing chunk mesh updates
     if (updatingStack.length > 0) {
 
-        Vector3i poppedValue = updatingStack[0];
+        ThreadChunkPackage updatingPackage = updatingStack[0];
         updatingStack.popFront();
         // writeln("Updating Chunk Mesh: ", poppedValue);
 
         // Ship them to the chunk generator process
-        internalUpdateChunkMesh(poppedValue);
+        internalUpdateChunkMesh(updatingPackage);
     }
 }
 
 while(!Window.externalShouldClose()) {
-    receive(
-        (ThreadChunkPackage newPackage) {
-            writeln("I GOT SOME DATA BOI");
-            // This does not block the main thread
-            int x = 0;
-            for (int i = 0; i < 1_000_000_000; i++) {
-                x = x + i * 2;
-            }
-        },
-        // If you send this thread a bool, it continues, then breaks
-        (bool kill) {}
-    );
+
+    // A cpu saver routine
+    if (!didGenLastLoop) {
+
+        didGenLastLoop = false;
+        receive(
+            (ThreadChunkPackage newPackage) {
+                if (newPackage.updating) {
+                    writeln("this is an update!");
+                    updateChunkMesh(newPackage);
+                    didGenLastLoop = true;
+                } else {
+                    writeln("this is a new generation!");
+                    newChunkMeshUpdate(newPackage);
+                    didGenLastLoop = true;
+                }
+            
+            },
+            // If you send this thread a bool, it continues, then breaks
+            (bool kill) {}
+        );
+    } else {
+        didGenLastLoop = false;
+        receiveTimeout(
+            Duration(),
+            (ThreadChunkPackage newPackage) {
+                if (newPackage.updating) {
+                    writeln("this is an update!");
+                    updateChunkMesh(newPackage);
+                    didGenLastLoop = true;
+                } else {
+                    writeln("this is a new generation!");
+                    newChunkMeshUpdate(newPackage);
+                    didGenLastLoop = true;
+                }
+            
+            },
+            // If you send this thread a bool, it continues, then breaks
+            (bool kill) {}
+        );
+    }
+
+    if(updatingStack.length > 0 || newStack.length > 0) {
+        didGenLastLoop = true;
+        processChunkMeshUpdateStack();
+    }
 }
 
 writeln("thread mesh generator closed!");
