626e76ca71f16d0c85ff6c5e6ff63d3c42bb601d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-30 17:50:41 -0400

    Re-engineer ThreadChunkPackage

--- source/game/chunk/thread_chunk_package.d
+++ source/game/chunk/thread_chunk_package.d
@@ -1,21 +1,20 @@
 module game.chunk.thread_chunk_package;
 
 import game.chunk.chunk;
-import game.chunk.thread_message_chunk;
 
 // This is used strictly for the chunk mesh generator!
 // It's sole purpose is to tell the chunkmesh generator what the world is
 // so it can create mesh data
-shared struct ThreadChunkPackage {
+struct ThreadChunkPackage {
     bool exists = false;
-    ubyte yStack = 0;
-    ThreadMessageChunk thisChunk;
+    ubyte yStack = 0;    
     bool updating = false;
-
-    ThreadMessageChunk neighborNegativeX;
-    ThreadMessageChunk neighborPositiveX;
-    ThreadMessageChunk neighborNegativeZ;
-    ThreadMessageChunk neighborPositiveZ;
+    
+    Chunk thisChunk;
+    Chunk neighborNegativeX;
+    Chunk neighborPositiveX;
+    Chunk neighborNegativeZ;
+    Chunk neighborPositiveZ;
 
     // This accumulates converted chunks into one huge package
     this(Chunk thisChunk,
@@ -25,11 +24,11 @@ shared struct ThreadChunkPackage {
          Chunk neighborPositiveZ,
          ubyte yStack,
          bool updating) {
-            this.thisChunk = ThreadMessageChunk(thisChunk);
-            this.neighborNegativeX = ThreadMessageChunk(neighborNegativeX);
-            this.neighborPositiveX = ThreadMessageChunk(neighborPositiveX);
-            this.neighborNegativeZ = ThreadMessageChunk(neighborNegativeZ);
-            this.neighborPositiveZ = ThreadMessageChunk(neighborPositiveZ);
+            this.thisChunk = thisChunk;
+            this.neighborNegativeX = neighborNegativeX;
+            this.neighborPositiveX = neighborPositiveX;
+            this.neighborNegativeZ = neighborNegativeZ;
+            this.neighborPositiveZ = neighborPositiveZ;
             this.exists = true;
             this.yStack = yStack;
             this.updating = updating;
