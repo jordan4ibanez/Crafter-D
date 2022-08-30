0bd8f77613d1ffb12202d6b99eac88e80e920519
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-29 23:36:39 -0400

    Create thread_mesh_message.d

---
+++ source/game/graphics/thread_mesh_message.d
@@ -0,0 +1,28 @@
+module game.graphics.thread_mesh_message;
+
+shared struct ThreadMeshMessage {
+    float[] vertices;
+    int[] indices;
+    float[] textureCoordinates;
+    float[] colors;
+    string textureName;
+    this(float[] vertices, int[] indices, float[] textureCoordinates, float[] colors, string textureName) {
+        this.vertices = new float[vertices.length];
+        for (int i = 0; i < vertices.length; i++) {
+            this.vertices[i] = vertices[i];
+        }
+        this.indices = new int[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            this.indices[i] = indices[i];
+        }
+        this.textureCoordinates = new float[textureCoordinates.length];
+        for (int i = 0; i < textureCoordinates.length; i++) {
+            this.textureCoordinates[i] = textureCoordinates[i];
+        }
+        this.colors = new float[colors.length];
+        for (int i = 0; i < colors.length; i++) {
+            this.colors[i] = colors[i];
+        }
+        this.textureName = textureName;
+    }
+}
