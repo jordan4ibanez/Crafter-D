24ecbcbf57e31a0d41f8c7b0b958842ba2dc0a94
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-30 23:22:14 -0400

    Add the basics to frustum

--- source/engine/opengl/frustum_culling.d
+++ source/engine/opengl/frustum_culling.d
@@ -1,2 +1,14 @@
 module engine.opengl.frustum_culling;
 
+import vector_4d;
+import frustum_intersection;
+
+private immutable int NUMBER_OF_PLANES = 6;
+
+private Vector4d[] frustumPlanes = {
+    Vector4d[] temp;
+    for (int i = 0; i < NUMBER_OF_PLANES; i++) {
+        temp ~= Vector4d();
+    }
+    return temp;
+}();
