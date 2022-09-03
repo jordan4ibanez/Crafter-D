7c65ea4d28f1ac42841a194c188ea544db8de61a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-02 22:41:54 -0400

    Update player_factory.d

--- source/game/entity/player/player_factory.d
+++ source/game/entity/player/player_factory.d
@@ -5,18 +5,13 @@ import std.stdio;
 import game.entity.player.player;
 
 // A static factory/container class, allowing functional programming inside of OOP
-public static class PlayerFactory {
+private static Player[string] container;
 
-    private static Player[string] container;
+public static void spawnPlayer(Player player) {
+    this.container[player.getName()] = player;
+    writeln(player.classinfo.name , " HAS BEEN ADDED TO FACTORY: ", player.getUUID());
+}
 
-    public static void spawnPlayer(Player player) {
-        this.container[player.getName()] = player;
-        writeln(player.classinfo.name , " HAS BEEN ADDED TO FACTORY: ", player.getUUID());
-    }
-    
-    public static void debugFactory() {
-        writeln(this.container);
-    }
-    
-
+public static void debugFactory() {
+    writeln(this.container);
 }
