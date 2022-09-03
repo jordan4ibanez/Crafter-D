3352caff41cb352832e6ee027630e3c8e9ca6004
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-02 23:09:48 -0400

    imports & encapsulate client

--- source/game/client/player_client.d
+++ source/game/client/player_client.d
@@ -3,7 +3,11 @@ module game.client.player_client;
 import vector_3d;
 import vector_2d;
 import vector_2i;
+
 import Math = math;
+import Camera = engine.camera.camera;
+import Keyboard = engine.input.keyboard;
+import Mouse = engine.input.mouse;
 
 /*
 This is the best name I could come up with, the next choice was:
@@ -12,7 +16,8 @@ the_current_player_of_the_client_that_is_playing_the_game_right_now.d
 This is the player that is playing the game.
 */
 
-Vector3d position = *new Vector3d(0,0,0);
-Vector3d rotation = *new Vector3d(0,0,0);
-Vector2d size = *new Vector2d(0.35, 1.8);
-double height = 1.5;
+private Vector3d position = *new Vector3d(0,0,0);
+private Vector3d rotation = *new Vector3d(0,0,0);
+private Vector2d size = *new Vector2d(0.35, 1.8);
+private double height = 1.5;
+private bool inGame = true;
