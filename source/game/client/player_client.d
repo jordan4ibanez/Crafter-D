57bdcd3b836ba3f3bd5a8bbdd39c67862de1c997
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-02 23:04:02 -0400

    Give player some components

--- source/game/client/player_client.d
+++ source/game/client/player_client.d
@@ -1,6 +1,7 @@
 module game.client.player_client;
 
 import vector_3d;
+import vector_2d;
 import vector_2i;
 import Math = math;
 
@@ -8,6 +9,10 @@ import Math = math;
 This is the best name I could come up with, the next choice was:
 the_current_player_of_the_client_that_is_playing_the_game_right_now.d
 
-This is the player that is playing the game. 
-
+This is the player that is playing the game.
 */
+
+Vector3d position = *new Vector3d(0,0,0);
+Vector3d rotation = *new Vector3d(0,0,0);
+Vector2d size = *new Vector2d(0.35, 1.8);
+double height = 1.5;
