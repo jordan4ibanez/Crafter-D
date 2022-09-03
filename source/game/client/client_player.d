31f1a995ca34b81931ceee84b879dc51cf99a732
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-02 22:59:03 -0400

    Add basic imports/notes

--- source/game/client/client_player.d
+++ source/game/client/client_player.d
@@ -1,2 +1,13 @@
 module game.client.client_player;
 
+import vector_3d;
+import vector_2i;
+import Math = math;
+
+/*
+This is the best name I could come up with, the next choice was:
+the_current_player_of_the_client_that_is_playing_the_game_right_now.d
+
+This is the player that is playing the game. 
+
+*/
