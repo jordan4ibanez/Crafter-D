82b1882e8446f2ed2466215b5b8e7c9139bdd0b1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-06 19:41:26 -0400

    Rebuild chunk container into a ConcurrentHashMap

--- source/game/chunk/chunk_container.d
+++ source/game/chunk/chunk_container.d
@@ -14,6 +14,7 @@ import std.concurrency;
 import std.algorithm.mutation: copy;
 import core.time: Duration;
 import asdf;
+import core.sync.mutex;
 
 // Internal engine libraries
 import ThreadLibrary = engine.thread.thread_library;
@@ -33,140 +34,133 @@ properties to treat the entire file as a static class
 This is meant to be handled functionally
 */
 
-// Hashmap container for generated chunks
-private shared(Chunk[Vector2i]) container;
+ConcurrentHashMap chunkData = new ConcurrentHashMap();
 
-// External entry point into adding new chunks to the map
-void generateChunk(Vector2i position) {
-    send(ThreadLibrary.getWorldGeneratorThread(), position);
-}
+public shared synchronized class ConcurrentHashMap {
 
-// Gets a chunk from the container
-immutable(Chunk) getChunk(Vector2i position) {
-    synchronized {
-    if (position in container) {
-        Chunk original = cast(Chunk)container[position];
-        immutable Chunk clone = cast(immutable)original;
-        return clone;
+    // Hashmap container for generated chunks
+    private Chunk[Vector2i] container;
+
+    // External entry point into adding new chunks to the map
+    void generateChunk(Vector2i position) {
+        send(ThreadLibrary.getWorldGeneratorThread(), position);
     }
-    // writeln("WARNING, A GARBAGE CHUNK HAS BEEN DISPATCHED");
-    // Return non-existent chunk
-    return immutable Chunk();
+
+    // Gets a chunk from the container
+    immutable(Chunk) getChunk(Vector2i position) nothrow {
+        shared Chunk clone;
+        if (position in container) {
+            shared Chunk original = container[position];
+            clone = original;
+        }
+        // writeln("WARNING, A GARBAGE CHUNK HAS BEEN DISPATCHED");
+        // Return non-existent chunk
+        return cast (immutable)clone;
     }
-}
 
-// Gets a shared chunk from the container
-/*
-shared(Chunk) getSharedChunk(Vector2i position) {
-    if (position in container) {
-        Chunk original = cast(Chunk)container[position];
-        Chunk clone = original.clone();
-        return cast(shared(Chunk))clone;
+    // Gets a shared chunk from the container
+    /*
+    shared(Chunk) getSharedChunk(Vector2i position) {
+        if (position in container) {
+            Chunk original = cast(Chunk)container[position];
+            Chunk clone = original.clone();
+            return cast(shared(Chunk))clone;
+        }
+        // writeln("WARNING, A GARBAGE CHUNK HAS BEEN DISPATCHED");
+        // Return non-existent chunk
+        return cast(shared(Chunk))Chunk();
     }
-    // writeln("WARNING, A GARBAGE CHUNK HAS BEEN DISPATCHED");
-    // Return non-existent chunk
-    return cast(shared(Chunk))Chunk();
-}
-*/
+    */
 
-// Gets a mutable chunk from the container
-ref Chunk getMutableChunk(Vector2i position) {
-    if (position in container) {
-        return cast(Chunk)container[position];
+    // Gets a mutable chunk from the container
+    ref Chunk getMutableChunk(Vector2i position) {
+        if (position in container) {
+            return cast(Chunk)container[position];
+        }
+        // This is where serious problems could happen if existence is not checked
+        writeln("WARNING, A MUTABLE GARBAGE CHUNK HAS BEEN DISPATCHED");
+        // This becomes garbage data
+        return *new Chunk();
     }
-    // This is where serious problems could happen if existence is not checked
-    writeln("WARNING, A MUTABLE GARBAGE CHUNK HAS BEEN DISPATCHED");
-    // This becomes garbage data
-    return *new Chunk();
-}
 
-// Internal chunk generation dispatch
-// This will be reused to receive chunks from the world generator
+    // Internal chunk generation dispatch
+    // This will be reused to receive chunks from the world generator
 
-void receiveChunksFromWorldGenerator() {
-    synchronized{
-    bool received = true;
-    while(received) {
-        received = false;
-        receiveTimeout(
-            Duration(),
-            (immutable Chunk immutableGeneratedChunk) {
-                
-                received = true;
+    void receiveChunksFromWorldGenerator() {
+        bool received = true;
+        while(received) {
+            received = false;
+            receiveTimeout(
+                Duration(),
+                (immutable Chunk immutableGeneratedChunk) {
+                    
+                    received = true;
 
-                Chunk generatedChunk = cast(Chunk)immutableGeneratedChunk;
+                    Chunk generatedChunk = cast(Chunk)immutableGeneratedChunk;
 
 
-                Vector2i newPosition = generatedChunk.getPosition();
-                container[newPosition] = cast(shared(Chunk))generatedChunk;
+                    Vector2i newPosition = generatedChunk.getPosition();
+                    container[newPosition] = cast(shared(Chunk))generatedChunk;
 
-                // Finally add a new chunk mesh update into the chunk mesh generator
-                Tid cmg = ThreadLibrary.getChunkMeshGeneratorThread();
+                    // Finally add a new chunk mesh update into the chunk mesh generator
+                    Tid cmg = ThreadLibrary.getChunkMeshGeneratorThread();
 
-                for (ubyte y = 0; y < 8; y++) {
-                    // This creates A LOT of data, but hopefully it will not be too much for D
-                    // Dump it right into the chunk mesh generator thread
-                    send(cmg, MeshUpdate(Vector3i(newPosition.x, y, newPosition.y), true));
-                    // All that cloned chunk data goes into the other thread and we won't worry about it
-                    // Hopefully
-                }
-            },
-        );
-    }
-    }
-}
-
-void receiveMeshesFromChunkMeshGenerator() {
-    synchronized{
-    bool received = true;
-    while (received){
-        received = false;
-        receiveTimeout(
-            Duration(),
-            (shared(ThreadMeshMessage) newMesh) {               
-                received = true;
-
-                ThreadMeshMessage thisNewMesh = cast(ThreadMeshMessage) newMesh;
-
-                Vector3i position = thisNewMesh.position;
-
-                Chunk mutableChunk = getMutableChunk(Vector2i(position.x, position.z));
-
-                // New mesh is blank! Remove
-                if (thisNewMesh.vertices.length == 0) {        
-                    mutableChunk.removeMesh(position.y);
-                } else {
-                    mutableChunk.setMesh(position.y, Mesh(
-                        thisNewMesh.vertices,
-                        thisNewMesh.indices,
-                        thisNewMesh.textureCoordinates,
-                        thisNewMesh.colors,
-                        thisNewMesh.textureName
-                    ));
-                }
-            }
-        );
-    }
-    }
-}
-
-void renderWorld() {
-    getShader("main").setUniformI("textureSampler", 0);
-    getShader("main").setUniformF("light", 1);
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, getTexture("textures/world_texture_map.png"));
-
-    foreach (shared(Chunk) thisChunk; container) {
-        Chunk castedChunk = cast(Chunk) thisChunk;
-        for (ubyte i = 0; i < 8; i++) {
-            castedChunk.drawMesh(i);
+                    for (ubyte y = 0; y < 8; y++) {
+                        // This creates A LOT of data, but hopefully it will not be too much for D
+                        // Dump it right into the chunk mesh generator thread
+                        send(cmg, MeshUpdate(Vector3i(newPosition.x, y, newPosition.y), true));
+                        // All that cloned chunk data goes into the other thread and we won't worry about it
+                        // Hopefully
+                    }
+                },
+            );
         }
     }
-}
 
+    void receiveMeshesFromChunkMeshGenerator() {
+        bool received = true;
+        while (received){
+            received = false;
+            receiveTimeout(
+                Duration(),
+                (shared(ThreadMeshMessage) newMesh) {               
+                    received = true;
 
-// Everything past this is for debugging components of this static class
-void debugFactoryContainer() {
-    writeln("factory container: ", container);
+                    ThreadMeshMessage thisNewMesh = cast(ThreadMeshMessage) newMesh;
+
+                    Vector3i position = thisNewMesh.position;
+
+                    Chunk mutableChunk = getMutableChunk(Vector2i(position.x, position.z));
+
+                    // New mesh is blank! Remove
+                    if (thisNewMesh.vertices.length == 0) {        
+                        mutableChunk.removeMesh(position.y);
+                    } else {
+                        mutableChunk.setMesh(position.y, Mesh(
+                            thisNewMesh.vertices,
+                            thisNewMesh.indices,
+                            thisNewMesh.textureCoordinates,
+                            thisNewMesh.colors,
+                            thisNewMesh.textureName
+                        ));
+                    }
+                }
+            );
+        }
+    }
+
+    void renderWorld() {
+        getShader("main").setUniformI("textureSampler", 0);
+        getShader("main").setUniformF("light", 1);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, getTexture("textures/world_texture_map.png"));
+
+        foreach (shared(Chunk) thisChunk; container) {
+            Chunk castedChunk = cast(Chunk) thisChunk;
+            for (ubyte i = 0; i < 8; i++) {
+                castedChunk.drawMesh(i);
+            }
+        }
+    }
 }
