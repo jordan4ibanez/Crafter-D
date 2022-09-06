6234ed8377e06e4314862b6318f7a5319769edb0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-06 01:36:47 -0400

    Implement basics of collision movement

--- source/game/physics/collision_detection.d
+++ source/game/physics/collision_detection.d
@@ -2,6 +2,9 @@ module game.physics.collision_detection;
 
 import vector_2d;
 import vector_3d;
+import delta_time;
+
+import Math = math;
 
 // Basic AABB and magnetic cylinder, nothing fancy
 // If you would like to see how the AABB works with a visualization:
@@ -9,10 +12,33 @@ import vector_3d;
 
 // I dunno where to even begin with this so here we go
 
-void collideWithTerrain(ref Vector3d velocity, ){
+void collideWithTerrain(ref Vector3d position, ref Vector3d velocity, Vector2d size, double inertia){
+
+    double delta = getDelta();
+        
+    // velocity and position are a constant, delta is the frame interpolator
+
+    // This goes as move axis -> check axis, next axis, etc, y,x,z
+
+    // Y first for half slabs and steps, also the check if player is on ground
+
+    // Friction will be checked by doing a dry run collide with the floor then a real collide for situations like ice
+
+    position.y += velocity.y * delta;
+
+    // check goes here
+
+    position.x += velocity.x * delta;
+
+    // check goes here
+
+    position.z += velocity.z * delta;
+
+    // check goes here
 
 }
 
+/*
 private void applyVelocity() {
     double delta = getDelta();
     position.x += velocity.x * delta;
@@ -29,3 +55,4 @@ private void applyFriction() {
         velocity.zero();
     }
 }
+*/
