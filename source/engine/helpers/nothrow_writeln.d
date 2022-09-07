d1ebf8c3f38ad26487987be075ebbef78c849859
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-06 20:31:53 -0400

    Synchronize

--- source/engine/helpers/nothrow_writeln.d
+++ source/engine/helpers/nothrow_writeln.d
@@ -3,7 +3,9 @@ module engine.helpers.nothrow_writeln;
 import std.stdio: writeln;
 
 void nothrowWriteln(Exception input) nothrow {
+    synchronized {
     try {
         writeln(input);
     } catch(Exception){}
+    }
 }
